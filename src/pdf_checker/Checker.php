b27d648568b41ec74823341cf9fa3d51460260ff
MarcellTrixie <marcell.chelsea@gmail.com>
2019-09-23 23:24:18 +0700

    Update

--- src/pdf_checker/Checker.php
+++ src/pdf_checker/Checker.php
@@ -1,4 +1,13 @@
 <?php
+	
+	include "PS01_TypoChecker.php";
+	include "PS03_SpaceChecker.php";
+	include "PS05_CapitalLetterChecker.php";
+	include "PS09_SubChapterChecker.php";
+	include "KAL02_PrefaceChecker.php";
+	include "KAL03_ThesisDataChecker.php";
+	include "NAT01_ReferenceChecker.php";
+	include "VAN03_SubjectPronounChecker.php";
 
 	/* 
 	 * Kelas Parent untuk seluruh checker
@@ -9,15 +18,24 @@
 		/* 
 		 * Method untuk memeriksa kesalahan dalam dokumen skripsi
 		 */
-		public function __construct(){
-
+		public function errorChecking($pdf_extract){
+			
 		}
 
-		/* 
-		 * Method untuk memeriksa kesalahan dalam dokumen skripsi
+		/**
+		 * Mendapatkan seluruh checkers yang tersedia
+		 * @return Checkers[] seluruh checker yang bisa dipakai
 		 */
-		public function errorChecking($document){
-			
+		public static function getAllCheckers() {
+			return [
+				new PS01_TypoChecker(),
+				new PS03_SpaceChecker(),
+				new PS05_CapitalLetterChecker(),
+				new PS09_SubChapterChecker(),
+				new KAL03_ThesisDataChecker(),
+				new NAT01_ReferenceChecker(),
+				new VAN03_SubjectPronounChecker()
+			]
 		}
 
 	}
--- src/pdf_checker/Main.php
+++ src/pdf_checker/Main.php
@@ -1,10 +1,19 @@
 <?php
-
+	
+	include "../vendor/autoload.php";
 	include "PDFExtractor.php";
 
 	$file = $argv[1];
-	$pdfExtractor = new PDFExtractor();
-	$pdfExtractor->extractPDF($file);
-	$pdfExtractor->run();
+	$skripsi_extract = new SkripsiExtract();
+	$skripsi_extract->extractFromPDF($file);
+
+	$checkers = Checker::getAllCheckers();
+	$all_errors = [];			
+	for ($checkers as $checker) {
+		array_push($all_errors, $checker->errorChecking($skripsi_extract));
+	}
+	foreach ($all_errors as $error) {
+		echo $error;
+	}
 
 ?>
