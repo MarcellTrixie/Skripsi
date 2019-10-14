20f6ce6a3d72f655ae676056bc8874360f522752
MarcellTrixie <marcell.chelsea@gmail.com>
2019-10-14 21:41:47 +0700

    Update 14-10-2019

--- src/pdf_checker/Checkers/PS01_TypoChecker.php
+++ src/pdf_checker/Checkers/PS01_TypoChecker.php
@@ -10,8 +10,26 @@
 		/* 
 		 * Method untuk memeriksa kesalahan dalam dokumen skripsi
 		 */
-		public function errorChecking($document){
+		public function errorChecking($pdf_extract){
+			$file = fopen("../../dictionary/id_iD/id_ID.dic", "r");
+			$dictionary = explode("\n", fread($myfile,filesize("id_ID.dic")));
+			$array = explode(" ", $pdf_extract->getContentPage());
 			
+			foreach ($array as $row => $value) {
+				$row = $row+1;
+				$pattern;
+				if (preg_match($pattern, $value)) {
+					$result = [
+						"row     : $row",
+						"error   : Terdapat kata yang tidak sesuai dengan KBBI",
+						"excerpt : $value"
+					];
+					echo "\n";
+					foreach ($result as $res) {
+   						echo "$res \n";
+					}
+				}	
+			}
 		}
 
 	}
--- src/pdf_checker/Checkers/PS09_SubChapterChecker.php
+++ src/pdf_checker/Checkers/PS09_SubChapterChecker.php
@@ -10,8 +10,24 @@
 		/* 
 		 * Method untuk memeriksa kesalahan dalam dokumen skripsi
 		 */
-		public function errorChecking($document){
-
+		public function errorChecking($pdf_extract){
+			$array = explode(". ", $pdf_extract->getContentPage());
+			$result;
+			foreach ($array as $row => $value) {
+				$row = $row+1;
+				$pattern = "/^[0-9].[0-9]\s[A-Za-z0-9]/";
+				if (preg_match($pattern, $value)) {
+					$result = [
+						"row     : $row",
+						"error   : Hanya terdapat 1 subbab saja, lebih baik tidak perlu dibuat subbab",
+						"excerpt : $value"
+					];
+					echo "\n";
+					foreach ($result as $res) {
+   						echo "$res \n";
+					}
+				}	
+			}
 		}
 
 	}
