4072f8ddd8044931d68dde9b9ffa0545e2110184
MarcellTrixie <marcell.chelsea@gmail.com>
2019-10-14 18:33:03 +0700

    Update 14-10-2019

--- src/pdf_checker/Checker.php
+++ src/pdf_checker/Checker.php
@@ -1,13 +1,13 @@
 <?php
 	
-	include "PS01_TypoChecker.php";
-	include "PS03_SpaceChecker.php";
-	include "PS05_CapitalLetterChecker.php";
-	include "PS09_SubChapterChecker.php";
-	include "KAL02_PrefaceChecker.php";
-	include "KAL03_ThesisDataChecker.php";
-	include "NAT01_ReferenceChecker.php";
-	include "VAN03_SubjectPronounChecker.php";
+	include "checkers/PS01_TypoChecker.php";
+	include "checkers/PS03_SpaceChecker.php";
+	include "checkers/PS05_CapitalLetterChecker.php";
+	include "checkers/PS09_SubChapterChecker.php";
+	include "checkers/KAL02_PrefaceChecker.php";
+	include "checkers/KAL03_ThesisDataChecker.php";
+	include "checkers/NAT01_ReferenceChecker.php";
+	include "checkers/VAN03_SubjectPronounChecker.php";
 
 	/* 
 	 * Kelas Parent untuk seluruh checker
@@ -32,10 +32,11 @@
 				new PS03_SpaceChecker(),
 				new PS05_CapitalLetterChecker(),
 				new PS09_SubChapterChecker(),
+				new KAL02_PrefaceChecker(),
 				new KAL03_ThesisDataChecker(),
 				new NAT01_ReferenceChecker(),
 				new VAN03_SubjectPronounChecker()
-			]
+			];
 		}
 
 	}
